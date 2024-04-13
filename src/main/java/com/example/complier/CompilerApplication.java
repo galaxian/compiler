d7b9254e49b74f79e244df7445911ed9085f218c
Lee <94841127+galaxian@users.noreply.github.com>
2024-04-13 16:31:56 +0900

    feat: 클래스 명 수정
    ComplierApplication ->. CompilerApplication

--- src/main/java/com/example/complier/CompilerApplication.java
+++ src/main/java/com/example/complier/CompilerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ComplierApplication {
+public class CompilerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComplierApplication.class, args);
+		SpringApplication.run(CompilerApplication.class, args);
 	}
 
 }
